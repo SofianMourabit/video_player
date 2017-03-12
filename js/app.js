a7fab12a0fc05fd0f44188eb44a05749b88e0c46
Sofian Mourabit <smourabit@informationmapping.com>
2017-03-13 02:57:58 +0700

    refactor app.js for better readability and DRYer code

--- js/app.js
+++ js/app.js
@@ -1,9 +1,8 @@
 $('video').mediaelementplayer();
-let i = 1;
+let i = 0;
 let videoWrapper = document.querySelector('.video');
 let captionWrapper = document.querySelector('.container');
 let captionTime = [
-  [0, 0],
   [0, 4.130],
   [4.130, 7.535],
   [7.535, 11.270],
@@ -19,25 +18,38 @@ let captionTime = [
   [46.300, 49.270],
   [49.270, 53.760],
   [53.760, 57.780],
-  [57.780, 60.150],
-  [61, 62]
+  [57.780, 61]
 ];
 
-
+function highlightCaption(span){
+  span.style.color = '#42b983';
+  span.style.fontWeight = 'bold';
+}
+function resetCaption(span){
+  span.style.color = '#34495e';
+  span.style.fontWeight = 'normal';
+}
+// Click on caption text to nagivate video
 captionWrapper.addEventListener('click', (event) => {
-  let number = event.target.className;
-  video.setCurrentTime(captionTime[number][0]);
+  // Clear Highlight
+  let spans = document.querySelectorAll('main span');
+  for(let j = 0, l = spans.length; j < l; j++){
+    resetCaption(spans[j]);
+  }
+  // Jump video to the caption's start time
+  let startTime = event.target.className;
+  video.pause();
+  video.setCurrentTime(captionTime[startTime][0]);
+  i = startTime;
 });
-
+// Highlight captions
 video.addEventListener('playing', () => {
   video.addEventListener('timeupdate', () => {
-    let caption = document.getElementsByClassName(''+i+'')[0];
+    let caption = document.getElementsByClassName(i)[0];
     if (video.getCurrentTime() > captionTime[i][0] &&  video.getCurrentTime() < captionTime[i][1]){
-      caption.style.color = '#42b983';
-      caption.style.fontWeight = 'bold';
-    }else {
-      caption.style.color = 'black';
-      caption.style.fontWeight = 'normal';
+      highlightCaption(caption);
+    }else if (!video.paused){
+      resetCaption(caption);
       i++;
     }
   });
