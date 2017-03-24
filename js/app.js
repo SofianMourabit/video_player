767567b2bf79f2114f207f5f760da60aca033126
Sofian Mourabit <smourabit@informationmapping.com>
2017-03-24 21:16:08 +0700

    using data attributes instead of array

--- js/app.js
+++ js/app.js
@@ -1,52 +1,27 @@
-$('video').mediaelementplayer();
-let i = 0;
+'use strict';
+const video = document.getElementById('video');
 const captionWrapper = document.querySelector('.container');
-const span = document.querySelectorAll('main span');
-const captionTime = [
-  [0, 4.130],
-  [4.130, 7.535],
-  [7.535, 11.270],
-  [11.270, 13.960],
-  [13.960, 17.940],
-  [17.940, 22.370],
-  [22.370, 26.880],
-  [26.880, 30.920],
-  [30.920, 34.730],
-  [34.730, 39.430],
-  [39.430, 41.190],
-  [41.190, 46.300],
-  [46.300, 49.270],
-  [49.270, 53.760],
-  [53.760, 57.780],
-  [57.780, 61]
-];
-function highlightCaption(caption, hightlight = 'yes'){
-  if (hightlight == 'yes'){
-    caption.classList.add("highlight");
-  }else if(hightlight == 'clear') {
-    caption.classList.remove("highlight");
-  }
-}
+const captions = document.querySelectorAll('main span');
+
+
 // Click on caption text to nagivate video
 captionWrapper.addEventListener('click', (event) => {
-  // Clear Highlight
-  for(let j = 0; j < span.length; j+= 1){
-    if(span[j].className == "highlight"){
-      highlightCaption(span[j], 'clear');
-    }
-  }
-  // Jump video to the caption's start time
-  let startTime = event.target.id;
-  video.setCurrentTime(captionTime[startTime][0]);
-  i = startTime;
+  let newTime = event.target.getAttribute('data-start');
+  video.setCurrentTime(newTime);
 });
+
 // Highlight captions
 video.addEventListener('timeupdate', () => {
-  let caption = document.getElementById(i);
-  if (video.getCurrentTime() >= captionTime[i][0] &&  video.getCurrentTime() < captionTime[i][1]){
-    highlightCaption(caption);
-  }else{
-    highlightCaption(caption, 'clear');
-    i++;
+  for(let i = 0; i < captions.length; i++) {
+    let time = video.currentTime;
+    let captionStart = captions[i].getAttribute('data-start');
+    let captionEnd = captions[i].getAttribute('data-end');
+    console.log(time + ' ' + captionStart);
+    if (time >= captionStart &&  time <= captionEnd){
+      captions[i].className = 'highlight';
+      console.log('yes Im getting here');
+    }else{
+      captions[i].className = '';
+    }
   }
 });
