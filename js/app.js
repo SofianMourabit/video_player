d551f450196c9d53fb80e099442ee326f5e449d2
Sofian Mourabit <smourabit@informationmapping.com>
2017-03-24 22:10:41 +0700

    fixed small bug for caption selection

--- js/app.js
+++ js/app.js
@@ -6,14 +6,17 @@ const captions = document.querySelectorAll('main span');
 
 // Click on caption text to nagivate video
 captionWrapper.addEventListener('click', (event) => {
+  
   let newTime = event.target.getAttribute('data-start');
+  if(newTime){
   video.setCurrentTime(newTime);
+  }
 });
 
 // Highlight captions
 video.addEventListener('timeupdate', () => {
   for(let i = 0; i < captions.length; i++) {
-    let time = video.currentTime;
+    let time = video.getCurrentTime();
     let captionStart = captions[i].getAttribute('data-start');
     let captionEnd = captions[i].getAttribute('data-end');
     if (time >= captionStart &&  time <= captionEnd){
